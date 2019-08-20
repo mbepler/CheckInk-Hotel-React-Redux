98849d69f7a95c2d91e6831129cddd62b0ac997b
Marcelo Bepler Junior <marcelobeplerjunior@gmail.com>
2019-08-20 01:29:42 -0300

    Updates

---
+++ precache-manifest.fc2b6f433098f1aac93c9c1c75728608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d520b987814618a61281bf87ea4e5a7",
+    "url": "/CheckInk-Hotel-React-Redux/index.html"
+  },
+  {
+    "revision": "f63503b7d599ab710594",
+    "url": "/CheckInk-Hotel-React-Redux/static/css/2.f3b1ba88.chunk.css"
+  },
+  {
+    "revision": "140581c77c3f7f873a7a",
+    "url": "/CheckInk-Hotel-React-Redux/static/css/main.125c2b15.chunk.css"
+  },
+  {
+    "revision": "f63503b7d599ab710594",
+    "url": "/CheckInk-Hotel-React-Redux/static/js/2.8ef50021.chunk.js"
+  },
+  {
+    "revision": "140581c77c3f7f873a7a",
+    "url": "/CheckInk-Hotel-React-Redux/static/js/main.e978e8cd.chunk.js"
+  },
+  {
+    "revision": "4d7dfbf0c92d5c113407",
+    "url": "/CheckInk-Hotel-React-Redux/static/js/runtime~main.3c01c847.js"
+  }
+]);
